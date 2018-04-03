c213e163efa96e0b8745080e9528a4f1e610c9f1
Caroline Salib <carolinesalibc@gmail.com>
2018-04-02 21:54:40 -0300

    Change order of index list to desc

--- app/models/productivity.rb
+++ app/models/productivity.rb
@@ -2,5 +2,5 @@ class Productivity < ApplicationRecord
   belongs_to :user
 
   scope :by_user, ->(user) { where user_id: user }
-  scope :ordered, -> { order(day: :asc)}
+  scope :ordered, -> { order(day: :desc)}
 end
